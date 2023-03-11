de0ba890c7196ca331469001a1208f087fdbdead
门那粒沙 <994266136@qq.com>
2023-03-11 23:42:32 +0800

    笔记补充

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+### springboot设计策略
+#### 开箱即用
+
+####约定大于配置
+
 ### spring boot属性加载顺序
 1、命令行中传入的参数
 
@@ -7,7 +12,7 @@
 
 4、JAVA的属性，可以通过System.getProperties() 获得的内容
 
-5、操作系统的环境变量（注意：通过环境变量进行配置时，因为不支持"."，所以需要将其更换为"_"）
+5、操作系统的环境变量（注意：通过环境变量进行配置时，因为不支持"."，所以需要将其更换为"_"；代码可见：org.springframework.core.env.SystemEnvironmentPropertySource.resolvePropertyName）
 
 6、通过random.*配置的随机属性
 
@@ -23,7 +28,10 @@
 
 12、应用默认属性，使用SpringApplication.setDefaultProperties定义的内容
 
+源码：org.springframework.boot.context.config.ConfigFileApplicationListener
 
 ### logback-spring.xml配置slf4j日志
 能通过<springProperty标签来引用application.properties\
 logback.xml的加载顺序早于springboot的application.yml (或application.properties) 
+
+
--- base/src/test/java/pwd/allen/JunitTest.java
+++ base/src/test/java/pwd/allen/JunitTest.java
@@ -1,6 +1,21 @@
 package pwd.allen;
 
+import org.junit.Test;
 import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author lenovo
@@ -11,5 +26,24 @@ public class JunitTest {
     @RepeatedTest(5)
     public void repeat() {
         System.out.println("hello!");
+    }
+
+    @Test
+    public void test() {
+        Matcher feature = Pattern.compile("(feature|master)").matcher("feature-1.0.0.pwdan#123");
+        if (feature.matches()) {
+            System.out.println(feature.group(0));
+        }
+    }
+
+    /**
+     * {@link SpringFactoriesLoader#FACTORIES_RESOURCE_LOCATION}
+     * @throws IOException
+     */
+    @Test
+    public void loader() throws IOException {
+        // 获取spring.factories中配置的指定类
+        List<String> stringList = SpringFactoriesLoader.loadFactoryNames(SpringApplicationRunListener.class, getClass().getClassLoader());
+        System.out.println(stringList);
     }
 }
