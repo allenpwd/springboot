7f8f2e3d380a0222751e41ac2f32071baaae763a
pwdan <994266136@qq.com>
2023-03-24 17:11:37 +0800

    笔记补充

--- base/src/main/java/pwd/allen/base/listener/MyApplicationContextInitializer.java
+++ base/src/main/java/pwd/allen/base/listener/MyApplicationContextInitializer.java
@@ -4,7 +4,10 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
- * 允许我们在 Spring 容器启动前，对 ApplicationContext 进行进一步的配置和处理
+ * 作用：允许我们在 Spring 容器启动前，对 ApplicationContext 进行进一步的配置和处理
+ *
+ * 实现示例
+ *  springboot整合nacos：NacosConfigApplicationContextInitializer
  *
  * 回调时机：ioc容器fresh之前
  *
@@ -12,7 +15,6 @@ import org.springframework.context.ConfigurableApplicationContext;
  * （1）配置在META-INF/spring.factories中
  *      org.springframework.context.ApplicationContextInitializer=\
  *      pwd.allen.listener.MyApplicationContextInitializer
- * （2）
  *
  * @author 门那粒沙
  * @create 2019-09-01 21:35
