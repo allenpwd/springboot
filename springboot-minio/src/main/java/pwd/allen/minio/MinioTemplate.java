fce5f6d97b873095e4808cf30443450771c10128
门那粒沙 <994266136@qq.com>
2023-10-11 22:18:55 +0800

    笔记补充

--- springboot-minio/src/main/java/pwd/allen/minio/MinioTemplate.java
+++ springboot-minio/src/main/java/pwd/allen/minio/MinioTemplate.java
@@ -1,18 +1,17 @@
 package pwd.allen.minio;
 
 import io.minio.*;
-import io.minio.errors.*;
+import io.minio.http.Method;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pwd.allen.minio.config.MyMinioProperties;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @Component
+@Slf4j
 public class MinioTemplate {
 
     @Autowired
@@ -24,25 +23,37 @@ public class MinioTemplate {
 
     public MinioTemplate() {}
 
-    public void createBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InvalidResponseException, ServerException, ErrorResponseException, XmlParserException, InternalException {
-        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
-            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+    public void createBucket(String bucketName) {
+        try {
+            if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     /**
-     * 获取文件外链
+     * 获取文件外链，可供外网下载文件
      * @param bucketName bucket 名称
      * @param objectName 文件名称
-     * @param expires   过期时间 <=7
+     * @param expires   过期时间 单位s 默认7天
      * @return
      */
-    public String getObjectURL(String bucketName,String objectName,int expires) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, ErrorResponseException, ServerException, InvalidResponseException, XmlParserException, InternalException {
-        return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder()
-                .bucket(bucketName)
-                        .object(objectName)
-                        .expiry(expires)
-                .build());
+    public String getObjectURL(String bucketName, String objectName, int expires) {
+        try {
+            return minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs
+                            .builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .expiry(expires)
+                            .method(Method.GET)
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -51,8 +62,12 @@ public class MinioTemplate {
      * @param objectName
      * @return
      */
-    public InputStream getObject(String bucketName,String objectName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InvalidResponseException, InternalException, ErrorResponseException, ServerException, XmlParserException {
-        return minioClient.getObject(GetObjectArgs.builder().bucket(bucketName).object(objectName).build());
+    public InputStream getObject(String bucketName, String objectName) {
+        try {
+            return minioClient.getObject(GetObjectArgs.builder().bucket(bucketName).object(objectName).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -61,13 +76,21 @@ public class MinioTemplate {
      * @param objectName
      * @param stream
      */
-    public void putObject(String bucketName, String objectName, InputStream stream) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-        putObject(bucketName, objectName, stream, stream.available(), "application/octet-stream");
+    public void putObject(String bucketName, String objectName, InputStream stream) {
+        try {
+            putObject(bucketName, objectName, stream, stream.available(), "application/octet-stream");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void putObject(String bucketName, String objectName, InputStream stream, int size, String contextType) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-        createBucket(bucketName);
-        minioClient.putObject(PutObjectArgs.builder().bucket(bucketName).object(objectName).stream(stream, size, -1).contentType(contextType).build());
+    public void putObject(String bucketName, String objectName, InputStream stream, int size, String contextType) {
+        try {
+            createBucket(bucketName);
+            minioClient.putObject(PutObjectArgs.builder().bucket(bucketName).object(objectName).stream(stream, size, -1).contentType(contextType).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -75,8 +98,12 @@ public class MinioTemplate {
      * @param bucketName
      * @param objectName
      */
-    public void removeObject(String bucketName, String objectName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, ServerException, ErrorResponseException, InvalidResponseException, XmlParserException, InternalException {
-        minioClient.removeObject(RemoveObjectArgs.builder().bucket(bucketName).object(objectName).build());
+    public void removeObject(String bucketName, String objectName) {
+        try {
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(bucketName).object(objectName).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- springboot-minio/src/test/java/pwd/allen/minio/MinioTemplateTest.java
+++ springboot-minio/src/test/java/pwd/allen/minio/MinioTemplateTest.java
@@ -1,10 +1,15 @@
 package pwd.allen.minio;
 
+import cn.hutool.core.io.IoUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import pwd.allen.minio.config.MinioConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 /**
  * @author pwdan
@@ -13,33 +18,36 @@ import pwd.allen.minio.config.MinioConfig;
 @Import(MinioTemplate.class)
 @SpringBootTest(classes = MinioConfig.class)
 class MinioTemplateTest {
-
     @Autowired
     private MinioTemplate minioTemplate;
 
-
+    String buckName =  "my-bucket";
     @Test
     void createBucket() {
-        System.out.println(minioTemplate);
+        minioTemplate.createBucket(buckName);
     }
 
     @Test
     void getObjectURL() {
+        String objectURL = minioTemplate.getObjectURL(buckName, "application.yml", 60);
+        System.out.println(objectURL);
     }
 
     @Test
     void getObject() {
+        InputStream inputStream = minioTemplate.getObject(buckName, "application.yml");
+        System.out.println(IoUtil.read(inputStream, Charset.defaultCharset()));
     }
 
     @Test
-    void putObject() {
-    }
-
-    @Test
-    void testPutObject() {
+    void putObject() throws IOException {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("application.yml");
+        minioTemplate.putObject(buckName, "application.yml", inputStream);
+        inputStream.close();
     }
 
     @Test
     void removeObject() {
+        minioTemplate.removeObject(buckName, "application.yml");
     }
 }
