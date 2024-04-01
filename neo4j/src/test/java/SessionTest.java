78efce9a564f193e1df04931e9dfc6bc0a707264
门那粒沙 <994266136@qq.com>
2024-04-01 23:17:14 +0800

    neo4j

--- neo4j/src/test/java/SessionTest.java
+++ neo4j/src/test/java/SessionTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.neo4j.driver.*;
 import org.neo4j.driver.internal.InternalPath;
 import org.neo4j.driver.internal.value.PathValue;
+import org.neo4j.driver.types.Node;
 import org.neo4j.driver.types.Path;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,17 +30,39 @@ public class SessionTest {
         this.session = driver.session();
     }
 
+    /**
+     * 查询链路并打印
+     */
     @Test
     public void test() {
-        // 查询 Danny DeVito 和 Jack Nicholson 的关系（通过演出和导演两种关联关系关联），最大深度3层
-        String cypherSql = "MATCH pwd=(:Person{name:\"Danny DeVito\"})-[:ACTED_IN|DIRECTED*1..3]-(:Person{name:\"Jack Nicholson\"}) RETURN pwd";
+        // 查询 Danny DeVito 和 Jack Nicholson 的关系（通过演出和导演两种关联关系关联），最大深度4层
+        String cypherSql = "MATCH pwd=(:Person{name:\"Danny DeVito\"})-[:ACTED_IN|DIRECTED*1..4]-(:Person{name:\"Jack Nicholson\"}) RETURN pwd";
         List<Record> list = session.run(cypherSql).list();
-        for (Record record : list) {
+        // 一条record是Danny DeVito 和 Jack Nicholson 之间的一条链路
+        for (int i = 0; i < list.size(); i++) {
+            Record record = list.get(i);
             PathValue pv = (PathValue) record.get("pwd");
             InternalPath internalPath = (InternalPath) pv.asPath();
+            StringBuilder sb = new StringBuilder();
             for (Path.Segment segment : internalPath) {
-                System.out.println(String.format("%s%s-%s->%s%s", segment.start().labels(), segment.start().asMap(), segment.relationship().type(), segment.end().labels(), segment.end().asMap()));
+                if (sb.length() == 0) {
+                    sb.append(String.format("%s%s-%s->%s%s", segment.start().labels(), getDesc(segment.start()), segment.relationship().type(), segment.end().labels(), getDesc(segment.end())));
+                } else if (segment.relationship().startNodeId() == segment.start().id()) {
+                    sb.append(String.format("-%s->%s%s", segment.relationship().type(), segment.end().labels(), getDesc(segment.end())));
+                } else {
+                    sb.append(String.format("<-%s-%s%s", segment.relationship().type(), segment.end().labels(), getDesc(segment.end())));
+                }
             }
+            System.out.println(String.format("链路%d：%s", i + 1, sb.toString()));
+        }
+    }
+    private String getDesc(Node node) {
+        if (node.hasLabel("Person")) {
+            return node.get("name").asString();
+        } else if (node.hasLabel("Movie")) {
+            return node.get("title").asString();
+        } else {
+            return node.asMap().toString();
         }
     }
 }
