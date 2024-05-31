979e3b2d336a0e177562986cbb8a6575a6564b0c
pwdan <994266136@qq.com>
2024-05-31 17:27:47 +0800

    ApplicationListener

--- base/src/main/java/pwd/allen/base/listener/MyApplicationListener.java
+++ base/src/main/java/pwd/allen/base/listener/MyApplicationListener.java
@@ -1,18 +1,22 @@
 package pwd.allen.base.listener;
 
-import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.SpringApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 /**
- * TODO 没生效
+ *
+ * {@link org.springframework.boot.context.event.ApplicationStartedEvent}
+ * {@link org.springframework.boot.context.event.ApplicationReadyEvent}
+ * {@link org.springframework.boot.context.event.ApplicationPreparedEvent} TODO 这个监控不到
+ *
  * @author pwdan
  * @create 2024-05-31 15:53
  **/
 @Component
-public class MyApplicationListener implements ApplicationListener<ApplicationPreparedEvent> {
+public class MyApplicationListener implements ApplicationListener<SpringApplicationEvent> {
     @Override
-    public void onApplicationEvent(ApplicationPreparedEvent applicationPreparedEvent) {
-        System.out.println("ApplicationListener.onApplicationEvent...监听ApplicationPreparedEvent");
+    public void onApplicationEvent(SpringApplicationEvent event) {
+        System.out.println(String.format("ApplicationListener.onApplicationEvent...监听到事件:%s", event.getClass().getName()));
     }
 }
